42fd6321f7450a7cc246c95ff54edb0369e962a4
Tom Schwindl <schwindl@posteo.de>
2024-04-18 17:08:00 +0200

    include: improve indentation

--- include/converter.hpp
+++ include/converter.hpp
@@ -6,23 +6,23 @@
 #include "file.hpp"
 
 class Converter {
-	public:
-		Converter();
-		~Converter();
+public:
+	Converter();
+	~Converter();
 
-		bool parse_json(std::string);
-		bool write_bat();
-		void print_fmt();
-		void clear_file();
-		void out_to_file(bool);
-		void force_overwrite(bool);
-		bool overwrite(char *);
+	bool parse_json(std::string);
+	bool write_bat();
+	void print_fmt();
+	void clear_file();
+	void out_to_file(bool);
+	void force_overwrite(bool);
+	bool overwrite(char *);
 
-	private:
-		File *file;
-		Json::Reader reader;
-		bool writeToFile;
-		bool forceOW;
+private:
+	File *file;
+	Json::Reader reader;
+	bool writeToFile;
+	bool forceOW;
 };
 
 #endif /* CONVERTER_HPP_ */
--- include/file.hpp
+++ include/file.hpp
@@ -6,38 +6,38 @@
 #include <jsoncpp/json/json.h>
 
 class File {
-	public:
-		bool fill(Json::Value&, std::string, std::ostream&);
+public:
+	bool fill(Json::Value&, std::string, std::ostream&);
 
-		/* Formatted output. */
-		void iterate_env(std::ostream&, const std::string&, bool) const;
-		void iterate_commands(std::ostream&, const std::string&, bool) const;
-		void iterate_paths(std::ostream&, const std::string&, const std::string&) const;
-		std::string title() const;
+	/* Formatted output. */
+	void iterate_env(std::ostream&, const std::string&, bool) const;
+	void iterate_commands(std::ostream&, const std::string&, bool) const;
+	void iterate_paths(std::ostream&, const std::string&, const std::string&) const;
+	std::string title() const;
 
-		/* Manupulate fields after initial File::fill(). */
-		void overwrite_hideshell(bool);
-		void overwrite_application(std::string);
-		void overwrite_outfile(std::string);
+	/* Manupulate fields after initial File::fill(). */
+	void overwrite_hideshell(bool);
+	void overwrite_application(std::string);
+	void overwrite_outfile(std::string);
 
-		/* Receive information from outside. */
-		bool hideshell() const;
-		const std::string& application() const;
-		const std::string& outfile() const;
-		std::size_t env_size() const;
-		std::size_t paths_size() const;
-		std::size_t commands_size() const;
+	/* Receive information from outside. */
+	bool hideshell() const;
+	const std::string& application() const;
+	const std::string& outfile() const;
+	std::size_t env_size() const;
+	std::size_t paths_size() const;
+	std::size_t commands_size() const;
 
-	private:
-		std::list<std::string> m_commands;
-		std::list<std::string> m_keys;
-		std::list<std::string> m_values;
-		std::list<std::string> m_paths;
-		Json::Value m_entries;
-		std::string m_inFile;
-		std::string m_outFile;
-		std::string m_application;
-		bool m_hideshell;
+private:
+	std::list<std::string> m_commands;
+	std::list<std::string> m_keys;
+	std::list<std::string> m_values;
+	std::list<std::string> m_paths;
+	Json::Value m_entries;
+	std::string m_inFile;
+	std::string m_outFile;
+	std::string m_application;
+	bool m_hideshell;
 };
 
 #endif /* FILE_HPP_ */
